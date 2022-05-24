00809725b4a7d327d05a14f4d62cb94563763337
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-24 15:41:44 -0500

    assignment completed

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,44 @@
 const houses = require("./db.json");
 
+let houseID = 4;
+
 module.exports = {
-  getAllHouses: () => {
-    console.log(houses);
+  getAllHouses: (req, res) => {
+    res.status(200).send(houses);
   },
-  deleteHouse: () => {},
-  createHouse: () => {},
-  updateHouse: () => {},
+  deleteHouse: (req, res) => {
+    let index = houses.findIndex((item) => item.id === +req.params.id);
+    houses.splice(index, 1);
+    res.status(200).send(houses);
+  },
+  createHouse: (req, res) => {
+    let { address, price, imageURL } = req.body;
+    let newHouse = {
+      id: houseID,
+      address: address,
+      price: price,
+      imageURL: imageURL,
+    };
+    houses.push(newHouse);
+    houseID++;
+    res.status(200).send(houses);
+  },
+  updateHouse: (req, res) => {
+    console.log("hit");
+    let id = req.params.id;
+    let type = req.body.type;
+    let index = houses.findIndex((item) => item.id === +id);
+    if (type === "plus") {
+      houses[index].price += 10000;
+      res.status(200).send(houses);
+    } else if (type === "minus" && houses[index].price - 10000 < 0) {
+      houses[index].price = 0;
+      res.status(200).send(houses);
+    } else if (houses[index].price === 0) {
+      res.status(400).send("cannot price below 0");
+    } else if (type === "minus") {
+      houses[index].price -= 10000;
+      res.status(200).send(houses);
+    }
+  },
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const cors = require("cors");
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(cors());
 
 app.get("/api/houses", controllerFile.getAllHouses);
 app.post("/api/houses", controllerFile.createHouse);
-app.put("/api/houses/id", controllerFile.updateHouse);
+app.put("/api/houses/:id", controllerFile.updateHouse);
 app.delete("/api/houses/:id", controllerFile.deleteHouse);
 
 app.listen(4004, () => console.log(`running on 4004`));
