a2e3f7fb153d8f7be0f4f088b22fd5792edfea9f
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-24 14:06:34 -0500

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,10 @@
+const houses = require("./db.json");
+
+module.exports = {
+  getAllHouses: () => {
+    console.log(houses);
+  },
+  deleteHouse: () => {},
+  createHouse: () => {},
+  updateHouse: () => {},
+};
